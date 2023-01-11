12f948082020fb537f99977f4d7194b42f579679
rpm <rpm@rpms-MacBook-Pro.local>
2023-01-11 11:03:38 -0400

    initial registration support for ios

---
+++ app/じゅり/Codable/RegisterGetResponse.swift
@@ -0,0 +1,13 @@
+//
+//  RegisterGetResponse.swift
+//  じゅり
+//
+//  Created by rpm on 1/11/23.
+//
+
+import Foundation
+
+struct RegisterGetResponse: Codable {
+  var challenge: String
+  var uid: String
+}
